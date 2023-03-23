3e931ce20da632b76d21790f3173b442d5ebd827
lostinafro <saggese.fabio@gmail.com>
2023-03-23 16:05:30 +0100

    Fixed presentation and saving data

--- plot_results_rate_vs_tau.py
+++ plot_results_rate_vs_tau.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import rc
 
-from environment import ecdf, OUTPUT_DIR, TAU
+from environment import ecdf, OUTPUT_DIR, TAU, BANDWIDTH
 import scenario.common as cmn
 
 rc('font', **{'family': 'sans serif', 'serif': ['Computer Modern']})
@@ -13,29 +13,34 @@ setups = ['ob-cc', 'ib-wf']
 labels_setups = ['OB-CC', 'IB-CC']
 
 # Define paradigms
-paradigms = ['opt_ce_vs_tau_', 'cb_bsw_vs_tau_fixed_', 'cb_bsw_vs_tau_flexible_']
+paradigms = ['opt_ce_vs_tau_', 'cb_bsw_vs_tau_fixed_', 'cb_bsw_vs_tau_flexi_']
+colors=['black', 'blue', 'red']
 styles = ['-', '--', ':']
 labels = ['OPT-CE', 'CB-BSW: Fixed', 'CB-BSW: Flexible']
 
 colors_snr = ['blue', 'red']
 labels_snr = ['OPT-CE', 'CB-BSW']
 
-render = False
+render = True
 
-# generate plots with different tau
-fig_lo, axes_lo = plt.subplots(nrows=len(labels_setups))
-fig_hi, axes_hi = plt.subplots(nrows=len(labels_setups))
+# Define specific taus to plot
+taus = [15., 17.5, 20.]
+# define the axes for the specific taus
+plot_list = [plt.subplots(nrows=len(labels_setups)) for _ in taus]
+
+# plots varying tau
 fig_me, axes_me = plt.subplots(nrows=len(labels_setups))
 fig_snr, axes_snr = plt.subplots(figsize=(5, 2.5))
 fig_kpi, axes_kpi = plt.subplots()
 
+# Optimal KPI for BSW
 kpi_flexi = np.load('data/cb_bsw_opt_kpi.npz')['flexi']
 kpi_fixed = np.load('data/cb_bsw_opt_kpi.npz')['fixed']
 
-axes_kpi.plot(TAU, kpi_fixed, linewidth=1.5, linestyle=':', label=f'CB-BSW: Fixed', color='red')
-axes_kpi.plot(TAU, kpi_flexi, linewidth=1.5, linestyle='--', label=f'CB-BSW: Flexible', color='blue')
+axes_kpi.plot(TAU, kpi_fixed, linewidth=1.5, linestyle='--', label=f'CB-BSW: Fixed', color='blue')
+axes_kpi.plot(TAU, kpi_flexi, linewidth=1.5, linestyle=':', label=f'CB-BSW: Flexible', color='red')
 
-
+# Analysis for different CC and paradigms
 for ss, setup in enumerate(setups):
 
     for pp, paradigm in enumerate(paradigms):
@@ -53,40 +58,36 @@ for ss, setup in enumerate(setups):
             axes_snr.plot(10*np.log10(x_[::10]), y_[::10], linewidth=1.5, linestyle='--', label=f'{labels_snr[pp]}', color=colors_snr[pp])
 
 
-        ## RATE
+        ## RATE (FAKE)
         # Load data
-        rate = np.load(datafilename)['rate']
+        rate = BANDWIDTH * np.load(datafilename)['rate_real'] / 1e6
 
-        mean_rate_vs_tau = np.mean(rate, axis=1)
-        axes_me[ss].plot(TAU, mean_rate_vs_tau, linewidth=1.5, linestyle=styles[pp], label=labels[pp])
+        mean_rate_vs_tau =  np.mean(rate, axis=1)
+        axes_me[ss].plot(TAU, mean_rate_vs_tau, linewidth=1.5, linestyle=styles[pp], label=labels[pp], color=colors[pp])
 
-        # Get CDF for tau = 7.5 ms
-        x_, y_ = ecdf(rate[TAU == 8.125])
-        axes_lo[ss].plot(x_, y_, linewidth=1.5, linestyle=styles[pp], label=labels[pp])
+        # Get CDF for the specific taus
+        for tt, tau in enumerate(taus):
+            x_, y_ = ecdf(rate[TAU == tau])
+            plot_list[tt][1][ss].plot(x_, y_, linewidth=1.5, linestyle=styles[pp], label=labels[pp], color=colors[pp])
+            plot_list[tt][1][ss].set_title(labels_setups[ss])
 
-        # Get CDF for tau = 15 ms
-        x_, y_ = ecdf(rate[TAU == 10.])
-        axes_hi[ss].plot(x_, y_, linewidth=1.5, linestyle=styles[pp], label=labels[pp])
+# Printing plots
 
+# Rate CDF
+for tt, tau in enumerate(taus):
+    plot_list[tt][0].suptitle(r'CDF with $\tau =' + f'{tau:.1f}$ [ms]', fontsize=12)
+    cmn.printplot(plot_list[tt][0], plot_list[tt][1], render, filename=f'rate_cdf_tau{tau:.1f}', dirname=OUTPUT_DIR,
+                  labels=['$R$ [Mbit/s]', 'CDF', 'CDF', 'CDF'], orientation='vertical')
 
-    axes_me[ss].set_title(labels_setups[ss])
-    axes_lo[ss].set_title(labels_setups[ss])
-    axes_hi[ss].set_title(labels_setups[ss])
-
-fig_lo.suptitle(r'CDF with $\tau = 8.125$ ms', fontsize=12)
-fig_hi.suptitle(r'CDF with $\tau = 10$ ms', fontsize=12)
+# Average rate vs tau
 fig_me.suptitle(r'Average rate vs $\tau$', fontsize=12)
-
-
 cmn.printplot(fig_me, axes_me, render, filename='mean_rate', dirname=OUTPUT_DIR,
-              labels=[r'$\tau$ [ms]', '$R$ [bit/Hz/s]', '$R$ [bit/Hz/s]', '$R$ [bit/Hz/s]'], orientation='vertical')
-cmn.printplot(fig_lo, axes_lo, render, filename='rate_cdf_lo', dirname=OUTPUT_DIR,
-              labels=['$R$ [bit/Hz/s]', 'CDF', 'CDF', 'CDF'], orientation='vertical')
-cmn.printplot(fig_hi, axes_hi, render, filename='rate_cdf_hi', dirname=OUTPUT_DIR,
-              labels=['$R$ [bit/Hz/s]', 'CDF', 'CDF', 'CDF'], orientation='vertical')
+              labels=[r'$\tau$ [ms]', '$R$ [Mbit/s]', '$R$ [Mbit/s]', '$R$ [Mbit/s]'], orientation='vertical')
 
+# SNR CDF
 cmn.printplot(fig_snr, axes_snr, render, filename='snr_cdf', dirname=OUTPUT_DIR,
               labels=['$\gamma$ [dB]', 'CDF'])
 
+# Opt KPI for BSW
 cmn.printplot(fig_kpi, axes_kpi, render, filename='kpi_vs_tau', dirname=OUTPUT_DIR,
               labels=[r'$\tau$ [ms]', '$\gamma_0$ [dB]'])
